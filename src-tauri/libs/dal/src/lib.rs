84498921628d604ad0fd5d018ab5e8246f2e07e7
Kurisu <makise_kurisuu@outlook.jp>
2023-03-23 23:22:26 +0800

    ✨ 测试sqlite的接入

--- src-tauri/libs/dal/src/lib.rs
+++ src-tauri/libs/dal/src/lib.rs
@@ -1,30 +1,62 @@
+use sqlite::{Connection, Value};
+
+use crate::structs::server_info::ServerInfo;
+
 mod structs;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-pub fn open_sql() {
-    let connection = sqlite::open(":memory:").unwrap();
-    let query = "
-    CREATE TABLE users (name TEXT, age INTEGER);
-    INSERT INTO users VALUES ('Alice', 42);
-    INSERT INTO users VALUES ('Bob', 69);
-";
-    connection.execute(query).unwrap();
+pub fn open_sql() -> Connection {
+    let connection = sqlite::open("connections.db").unwrap();
+    let sql = "CREATE TABLE IF NOT EXISTS connections (
+                _id              INTEGER    primary key AUTOINCREMENT,
+                name             TEXT    not null,
+                host             TEXT    NOT NULL,
+                port             INTEGER default 6379,
+                username         TEXT    ,
+                password         TEXT    ,
+                cluster          INT     default 0,
+                nodes            TEXT    default '',
+                security_type    INT     default 0,
+                use_private_key  INT     default 0,
+                ssh_username     TEXT    ,
+                ssh_host         TEXT    ,
+                ssh_port         INTEGER default 6379,
+                ssh_password     TEXT    ,
+                private_key_path TEXT
+        )";
+    connection.execute(sql).unwrap();
+    connection
 }
 
+pub fn save_sql(con: Connection, info: ServerInfo) {
+    let sql = "INSERT INTO connections (
+                     name, host, port)
+                     values (:name, :host, :port)";
+    let mut statement = con.prepare(sql).unwrap();
+    statement.bind::<&[(_, Value)]>(&[
+        (":name", info.name.into()),
+        (":host", info.host.into()),
+        (":port", (info.port as i64).into()),
+    ][..]).unwrap();
+    statement.next().unwrap();
+}
 
 
 #[cfg(test)]
 mod tests {
     use crate::structs::server_info::ServerInfo;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let info = ServerInfo::default();
-        println!("{:?}", info);
+        let conn = open_sql();
+        let info = ServerInfo::create("test".to_string(), "127.0.0.1".to_string(), 6379, None, None);
+
+        save_sql(conn, info);
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
--- src-tauri/libs/dal/src/structs/server_info.rs
+++ src-tauri/libs/dal/src/structs/server_info.rs
@@ -1,21 +1,63 @@
+use serde_derive::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct ServerInfo {
-    /// 服务器ip/域名
+    pub _id: Option<i32>,
+    // 名称
+    pub name: String,
+    // 服务器ip/域名
     pub host: String,
-    /// 端口号
+    // 端口号 default 6379
     pub port: i32,
-    /// 密码
-    pub password: String,
+    // 用户名
+    pub username: Option<String>,
+    // 密码
+    pub password: Option<String>,
+    // 集群
+    pub cluster: Option<i32>,
+    // 集群节点
+    pub nodes: Option<String>,
+    // 安全类型 0: 不使用 1：ssl/tls  2：ssh tunnel
+    #[serde(rename="securityType")]
+    pub security_type: Option<i32>,
+    // 是否使用私钥
+    #[serde(rename="usePrivateKey")]
+    pub use_private_key: Option<bool>,
+    // ssh 隧道的用户名
+    #[serde(rename="sshUsername")]
+    pub ssh_username: Option<String>,
+    // ssh 隧道的地址
+    #[serde(rename="sshHost")]
+    pub ssh_host: Option<String>,
+    // ssh隧道的端口号 默认22
+    #[serde(rename="sshPort")]
+    pub ssh_port: Option<i32>,
+    // ssh隧道密码 私钥是为私钥密码
+    #[serde(rename="sshPassword")]
+    pub ssh_password: Option<String>,
+    // 私钥文件路径
+    #[serde(rename="privateKeyPath")]
+    pub private_key_path: Option<String>,
 }
 
 impl ServerInfo {
-    pub fn default() -> Self {
-        let info = ServerInfo {
-            host: "127.0.0.1".to_string(),
-            port: 6379,
-            password: "".to_string(),
-        };
-        return info;
+    pub fn create(name: String, host: String, port: i32, password: Option<String>, username: Option<String>) -> ServerInfo {
+        ServerInfo {
+            _id: None,
+            name,
+            host,
+            port,
+            username,
+            password,
+            cluster: None,
+            nodes: None,
+            security_type: None,
+            use_private_key: None,
+            ssh_username: None,
+            ssh_host: None,
+            ssh_port: None,
+            ssh_password: None,
+            private_key_path: None,
+        }
     }
 }
