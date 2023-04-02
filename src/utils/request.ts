1e4c87eda32d7ebd25974bf4653e79c96225b4f5
Kurisu <makise_kurisuu@outlook.jp>
2023-04-03 01:09:30 +0800

    🎨 重新定义返回类型

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,23 +4,11 @@ export interface CommArgs {
   [key: string]: unknown;
 }
 
-interface Message<T> {
-  data?: T;
-  code: number;
-  success: boolean;
-  msg: string;
-}
-
 export default async function request<T>(api: string, args?: CommArgs) {
   return new Promise<T | undefined>((resolve, reject) => {
-    invoke<Message<T>>(api, args)
+    invoke<T>(api, args)
       .then((res) => {
-        if (res.success) {
-          resolve(res.data);
-        } else {
-          console.log(res);
-          reject(new Error(res.msg));
-        }
+        resolve(res);
       })
       .catch((e) => {
         console.log(e);
