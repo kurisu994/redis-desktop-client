47cbe192b724c99ac14c9e282e0fdcd2a491b73d
Kurisu <lucifer994@gmail.com>
2024-04-21 22:53:07 +0800

    🚚 chore(*): 更新依赖

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use crate::common::cmd::*;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 use tauri::{GlobalWindowEvent, Manager, Theme, WindowEvent, Wry};
 
@@ -50,22 +51,22 @@ fn main() {
         .on_system_tray_event(ui::tray::Tray::on_system_tray_event)
         .on_window_event(handle_window_event)
         .invoke_handler(tauri::generate_handler![
-            common::cmd::all_server,
-            common::cmd::save_server,
-            common::cmd::copy_server,
-            common::cmd::delete_server,
-            common::cmd::test_server,
-            common::cmd::query_setting,
-            common::cmd::update_setting,
-            common::cmd::read_redis_dbs,
-            common::cmd::read_redis_status,
-            common::cmd::read_redis_key_list,
-            common::cmd::read_redis_key_tree,
-            common::cmd::close_redis,
-            common::cmd::read_redis_value,
-            common::cmd::update_redis_key_ttl,
-            common::cmd::delete_redis_key,
-            common::cmd::rename_key,
+            all_server,
+            save_server,
+            copy_server,
+            delete_server,
+            test_server,
+            query_setting,
+            update_setting,
+            read_redis_dbs,
+            read_redis_status,
+            read_redis_key_list,
+            read_redis_key_tree,
+            close_redis,
+            read_redis_value,
+            update_redis_key_ttl,
+            delete_redis_key,
+            rename_key,
         ])
         .run(context)
         .expect("error while running tauri application");
